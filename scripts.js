44182c46030ad0db866bce676af7ee0f65123ff7
Alex Ross <alex.ross@shopify.com>
2021-09-27 20:00:37 -0500

    displayed moods with button using javascript

--- scripts.js
+++ scripts.js
@@ -3,23 +3,23 @@ const itemsContainer = document.querySelector('#items')
 
 
 for (let i = 0; i < data.length; i += 1) {
-	// create a new div element and give it a class name
 	const newDiv = document.createElement('div');
 	newDiv.className = 'item'
-	// create an image element
 	const img = document.createElement('img');
-	// this will change each time we go through the loop. Can you explain why?
 	img.src = data[i].image
 	img.width = 300
 	img.height = 300
-	// Add the image to the div
 	newDiv.appendChild(img)
-   const desc = document.createElement('P');
-   desc.innerText = data[i].desc
-   newDiv.appendChild(desc)
-   const price = document.createElement('P');
-   price.innerText = data[i].price
-   newDiv.appendChild(price)
-	// put new div inside items container
 	itemsContainer.appendChild(newDiv)
+	const desc = document.createElement('P')
+	desc.innerText = data[i].desc
+	newDiv.appendChild(desc)
+	const price = document.createElement('P')
+	price.innerText = data[i].price
+	newDiv.appendChild(price)
+	const button = document.createElement('button')
+	button.id = data[i].name
+	button.dataset.price = data[i].price
+	button.innerHTML = "Add to Cart"
+	newDiv.appendChild(button)
 }
